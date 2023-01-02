d6ee9a2c5b7dff1a976883d5cefced2160263b87
christopher-chris <christopher.chris.ing@gmail.com>
2023-01-02 00:57:07 +0000

    wip

--- main.go
+++ main.go
@@ -1,5 +1,42 @@
 package main
 
+import (
+	"fmt"
+	"log"
+
+	"github.com/libp2p/go-libp2p"
+	"github.com/libp2p/go-libp2p-core/peer"
+	"github.com/libp2p/go-libp2p/p2p/protocol/circuitv2/relay"
+)
+
 func main() {
-	// ...
+	startRelay()
+}
+
+func startRelay() {
+	// Create a host to act as a middleman to relay messages on our behalf
+	relay1, err := libp2p.New()
+	if err != nil {
+		log.Printf("Failed to create relay1: %v", err)
+		return
+	}
+
+	// Configure the host to offer the ciruit relay service.
+	// Any host that is directly dialable in the network (or on the internet)
+	// can offer a circuit relay service, this isn't just the job of
+	// "dedicated" relay services.
+	// In circuit relay v2 (which we're using here!) it is rate limited so that
+	// any node can offer this service safely
+	_, err = relay.New(relay1)
+	if err != nil {
+		log.Printf("Failed to instantiate the relay: %v", err)
+		return
+	}
+
+	relay1info := peer.AddrInfo{
+		ID:    relay1.ID(),
+		Addrs: relay1.Addrs(),
+	}
+
+	fmt.Printf("Relay 1 info: %v", relay1info.Addrs)
 }
